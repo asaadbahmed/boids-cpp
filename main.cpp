31ee41f66ad403f4e8b39f7620f756203af5b53c
ace <asaadbinahmed@gmail.com>
2024-10-31 23:14:39 -0400

    Changed Boid Class to Struct
    Boid is now a struct instead of a class
    Wrote some boid generation code

--- main.cpp
+++ main.cpp
@@ -1,58 +1,52 @@
-#include <cmath>
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 class Vector2 {
 public:
-    double x {};
-    double y {};
+    double X {};
+    double Y {};
 
     Vector2(const double x = 0.0, const double y = 0.0) {
-        this -> x = x;
-        this -> y = y;
+        this -> X = x;
+        this -> Y = y;
     }
 
     Vector2 Unit() {
         const double magnitude = Magnitude();
-        return magnitude == 0.0 ? Vector2(0, 0) : Vector2(x / magnitude, y / magnitude);
+        return magnitude == 0.0 ? Vector2(0, 0) : Vector2(X / magnitude, Y / magnitude);
     }
 
     double Magnitude() {
-        return sqrt(pow(x, 2.0) + pow(y, 2.0));
+        return sqrt(pow(X, 2.0) + pow(Y, 2.0));
     }
 
     double Dot(const Vector2 vector) {
-        return (x * vector.x) + (y * vector.y);
+        return (X * vector.X) + (Y * vector.Y);
     }
 
     bool Equals(const Vector2 vector) {
-        return x == vector.x && y == vector.y;
+        return X == vector.X && Y == vector.Y;
     }
-};
-
-class Boid {
-    public:
-        Vector2 position {};
-        Vector2 velocity {};
-        Vector2 acceleration {};
-
-        Boid(const Vector2 position, const Vector2 velocity, const Vector2 acceleration) {
-            this -> position = position;
-            this -> velocity = velocity;
-            this -> acceleration = acceleration;
-        }
 };
 
 bool Expect(const double actual, const double expect, const double epsilon = 1e-9) {
     return fabs(actual - expect) < epsilon;
 }
 
-void Log(const std::string& testName, const bool success) {
+void Log(const string& testName, const bool success) {
     if (success) {
-        std::cout << "\033[32m" << testName << " test passed." << "\033[0m" << std::endl;
+        cout << "\033[32m" << testName << " test passed." << "\033[0m" << endl;
     } else {
-        std::cout << "\033[31m" <<  testName << " test failed." << "\033[0m" << std::endl;
+        cout << "\033[31m" <<  testName << " test failed." << "\033[0m" << endl;
     }
 }
+
+struct Boid {
+    Vector2 Position {};
+    Vector2 Velocity {};
+};
 
 int main() {
     Vector2 v1(1, 1);
@@ -63,6 +57,26 @@ int main() {
     Log("Unit vector", Expect(v2.Unit().Magnitude(), 1));
     Log("Vector dot-product", Expect(v1.Dot(v2), 4));
     Log("Vector equals", Expect(v1.Equals(v2), false));
+
+    vector<Boid> boids;
+    for (int i = 0; i < 100; i++) {
+        const int posX = rand() % 10 + 1;
+        const int posY = rand() % 10 + 1;
+        const int velX = rand() % 5 + 1;
+        const int velY = rand() % 5 + 1;
+
+        Boid boid {
+            Vector2(posX, posY),
+            Vector2(velX, velY)
+        };
+
+        boids.push_back(boid);
+    }
+
+    for (int i = 0; i < boids.size(); i++) {
+        Boid boid = boids.at(i);
+        printf("ID: %d, Position: (%f, %f), Velocity: (%f, %f)\n", i, boid.Position.X, boid.Position.Y, boid.Velocity.X, boid.Velocity.Y);
+    }
 
     return 0;
 }
